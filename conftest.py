c81b987eaa88a9904f6200eee96ac81af9cf9b57
Sergey Chukin <chuk2208@yandex.ru>
2024-06-23 14:37:08 +0500

    Исправлено

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 from data import Urls, StellarBurgerAuth
+from pages.page_private_area import PrivateAreaPage
 from locators.private_area_locators import PrivateAreaPageLocators
 
 
@@ -23,10 +24,10 @@ def driver(request):
 
 @pytest.fixture
 def authorized_driver(driver):
-    driver.find_element(*PrivateAreaPageLocators.private_area_link).click()
-    driver.find_element(*PrivateAreaPageLocators.login_email_input).send_keys(StellarBurgerAuth.EMAIL)
-    driver.find_element(*PrivateAreaPageLocators.login_password_input).send_keys(StellarBurgerAuth.PASSWORD)
-    driver.find_element(*PrivateAreaPageLocators.login_button_submit).click()
-    WebDriverWait(driver, 3).until(
-            expected_conditions.visibility_of_element_located(PrivateAreaPageLocators.login_expected_text))
+    page_authorized = PrivateAreaPage(driver)
+    page_authorized.button_click(PrivateAreaPageLocators.private_area_link)
+    page_authorized.input_data(PrivateAreaPageLocators.login_email_input, StellarBurgerAuth.EMAIL)
+    page_authorized.input_data(PrivateAreaPageLocators.login_password_input, StellarBurgerAuth.PASSWORD)
+    page_authorized.button_click(PrivateAreaPageLocators.login_button_submit)
+    page_authorized.wait_open_form(PrivateAreaPageLocators.login_expected_text)
     return driver
